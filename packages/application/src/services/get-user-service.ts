76038de990d0b63426f002894eae15fff0a47c94
Ben Wainwright <bwainwright28@gmail.com>
2025-11-15 10:07:56 +0000

    correct type error

--- packages/application/src/services/get-user-service.ts
+++ packages/application/src/services/get-user-service.ts
@@ -5,8 +5,8 @@ import type { User } from "@ynab-plus/domain";
 
 import { AbstractApplicationService } from "./abstract-application-service.ts";
 
-export class GetUserService extends AbstractApplicationService<"GetUser"> {
-  public override readonly commandName = "GetUser";
+export class GetUserCommandService extends AbstractApplicationService<"GetUserCommand"> {
+  public override readonly commandName = "GetUserCommand";
 
   public override requiredPermissions: ("public" | "user" | "admin")[] = [
     "admin",
@@ -23,7 +23,7 @@ export class GetUserService extends AbstractApplicationService<"GetUser"> {
 
   protected override async handle({
     command,
-  }: IHandleContext<"GetUser">): Promise<User | undefined> {
+  }: IHandleContext<"GetUserCommand">): Promise<User | undefined> {
     const { username } = command.data;
 
     const user = await this.users.get(username);
