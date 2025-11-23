95f14bc052e037337856eb565c6c680525ffdc2f
Ben Wainwright <bwainwright28@gmail.com>
2025-11-23 22:52:04 +0000

    add some helper test

--- packages/frontend/app/components/bank-integration-status/select-institution-button.tsx
+++ packages/frontend/app/components/bank-integration-status/select-institution-button.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Button, Flex, Modal } from "@mantine/core";
+import { Text, Autocomplete, Button, Flex, Modal } from "@mantine/core";
 import type { BankConnection } from "@ynab-plus/domain";
 import { useState } from "react";
 import { getOptionRenderer } from "./get-option-renderer.tsx";
@@ -27,6 +27,10 @@ export const SelectInstitutionButton = ({
           onChange={setBank}
           value={bank ?? ""}
         />
+        <Text mt="md">
+          Click on the box above and enter the name of your bank, then press the
+          connect button to be taken to your bank for authorization.
+        </Text>
         <Button mt="md" disabled={!bank}>
           Connect
         </Button>
