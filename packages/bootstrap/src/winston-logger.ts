cc0674aa52c109e2f444bea5ad83bc05631adbdd
Ben Wainwright <bwainwright28@gmail.com>
2025-12-01 23:39:57 +0000

    added better dev logging

--- packages/bootstrap/src/winston-logger.ts
+++ packages/bootstrap/src/winston-logger.ts
@@ -1,8 +1,27 @@
 import winston from "winston";
+import winstonDevConsole from "@epegzz/winston-dev-console";
 
 import type { ILogger } from "./i-logger.ts";
 
 export const getWinstonLogger = (): ILogger => {
+  if (process.env["NODE_ENV"] !== "production") {
+    const logger = winston.createLogger({
+      level: process.env["YNAB_PLUS_LOG_LEVEL"] ?? "info",
+    });
+
+    const devConsole =
+      winstonDevConsole as unknown as typeof winstonDevConsole.default;
+
+    logger.add(
+      devConsole.transport({
+        showTimestamps: true,
+        addLineSeparation: true,
+      }),
+    );
+
+    return logger;
+  }
+
   const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: "user-service" },
@@ -11,23 +30,6 @@ export const getWinstonLogger = (): ILogger => {
       new winston.transports.File({ filename: "combined.log" }),
     ],
   });
-
-  if (process.env["NODE_ENV"] !== "production") {
-    const logFormat = winston.format.printf(function (info) {
-      if (info.level === "error") {
-        return `${info.level}: ${String(info.message)} [${String(info["context"])}]`;
-      }
-
-      return `${info.level}: ${String(info.message)} [${String(info["context"])}]`;
-    });
-
-    logger.add(
-      new winston.transports.Console({
-        level: process.env["YNAB_PLUS_LOG_LEVEL"] ?? "info",
-        format: winston.format.combine(winston.format.colorize(), logFormat),
-      }),
-    );
-  }
 
   return logger;
 };
