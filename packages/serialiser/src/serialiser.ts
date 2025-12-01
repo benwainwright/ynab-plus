6eca60f19749f1e8a07d9da816a7326c047dfccc
Ben Wainwright <bwainwright28@gmail.com>
2025-12-01 19:55:48 +0000

    add all the types to the serialiser

--- packages/serialiser/src/serialiser.ts
+++ packages/serialiser/src/serialiser.ts
@@ -1,50 +1,56 @@
 import {
   Account,
+  BankConnection,
   SyncDetails,
   User,
+  RegularTask,
+  OauthToken,
   type IAccount,
   type ISyncDetails,
+  type IBankConnection,
   type IUser,
+  type IOauthToken,
+  type IRegularTask,
+  type SchedulableTask,
 } from "@ynab-plus/domain";
 import { Typeson } from "typeson";
 
 export class Serialiser {
   private registry = new Typeson();
 
+  private identifier = () => {};
+
   public constructor() {
     this.registry.register({
       syncDetails: [
         (thing) => thing instanceof SyncDetails,
-        (details: SyncDetails): ISyncDetails => ({
-          id: details.id,
-          checkpoint: details.checkpoint,
-          lastSync: details.lastSync,
-          provider: details.provider,
-        }),
+        (details: SyncDetails) => details.freezeDry(),
         (raw: ISyncDetails) => SyncDetails.reconstitute(raw),
       ],
       account: [
         (thing) => thing instanceof Account,
-        (account: Account): IAccount => ({
-          id: account.id,
-          userId: account.userId,
-          name: account.name,
-          type: account.type,
-          closed: account.closed,
-          note: account.note,
-          deleted: account.deleted,
-        }),
+        (account: Account) => account.freezeDry(),
         (raw: IAccount) => Account.reconstitute(raw),
+      ],
+      task: [
+        (thing) => thing instanceof RegularTask,
+        (task: RegularTask) => task.freezeDry(),
+        (raw: IRegularTask<SchedulableTask>) => RegularTask.reconstitute(raw),
       ],
       user: [
         (thing) => thing instanceof User,
-        (user: User): IUser => ({
-          id: user.id,
-          permissions: user.permissions,
-          email: user.email,
-          passwordHash: user.passwordHash,
-        }),
+        (user: User) => user.freezeDry(),
         (raw: IUser) => User.reconstitute(raw),
+      ],
+      bankConnection: [
+        (thing) => thing instanceof BankConnection,
+        (connection: BankConnection) => connection.freezeDry(),
+        (raw: IBankConnection) => BankConnection.reconstite(raw),
+      ],
+      token: [
+        (thing) => thing instanceof OauthToken,
+        (token: OauthToken) => token.freezeDry(),
+        (raw: IOauthToken) => OauthToken.reconstitute(raw),
       ],
     });
   }
