b41559927ec772d44931bd5dd3882dba60c61e07
Ben Wainwright <bwainwright28@gmail.com>
2025-12-13 20:13:18 +0000

    add related

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,9 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.{js,ts,tsx,jsx,json}": ["oxfmt --no-error-on-unmatched-pattern", "oxlint --type-aware --fix"],
+  "*.{js,ts,tsx,jsx,json}": [
+    "oxfmt --no-error-on-unmatched-pattern",
+    "oxlint --type-aware --fix",
+    "vitest related",
+  ],
 };
