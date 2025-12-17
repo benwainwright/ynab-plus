432a4f6e22180e41a2551e7fc8e9473ce1cfcf5a
Ben Wainwright <bwainwright28@gmail.com>
2025-12-17 07:33:07 +0000

    remove unneeded file

--- packages/domain/src/oauth-token/i-outh-token.ts
+++ packages/domain/src/oauth-token/i-outh-token.ts
@@ -1,12 +1,9 @@
 import z from "zod";
 
 export const oAuthTokenSchema = z.object({
-  expiry: z.union([
-    z
-      .union([z.string(), z.date()])
-      .transform((arg) => (typeof arg === "string" ? new Date(arg) : arg)),
-    z.undefined()
-  ]),
+  expiry: z
+    .union([z.string(), z.date()])
+    .transform((arg) => (typeof arg === "string" ? new Date(arg) : arg)),
   token: z.string(),
   refreshToken: z.string(),
   provider: z.string(),
